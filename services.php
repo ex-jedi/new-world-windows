3fda1e6ece0583a5823fd2623af8a4b235a01274
Mark <mark@pixelsmiths.co.uk>
2021-01-04 15:53:15 +0000

    Site pages

---
+++ services.php
@@ -0,0 +1,10 @@
+<?php include('perch/runtime.php'); ?>
+<?php perch_layout('main-header'); ?>
+<main class="main-content homepage-main-content" id="main-content">
+<h1>Ahoy</h1>
+</main>
+<?php perch_layout('main-footer'); ?>
+<?php perch_layout('main-footer-end-js'); ?>
+<!-- TODO: Remove debug -->
+<!-- <?php PerchUtil::output_debug(); ?> -->
+
