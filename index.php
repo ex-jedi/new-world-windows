524fa923984a886fcff49fc13f56ebf35e693384
Mark <mark@pixelsmiths.co.uk>
2021-01-04 17:18:22 +0000

    Split text and image section

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
-<h1>Ahoy</h1>
+  <?php perch_content("Section One"); ?>
+  <?php perch_content("Parallax Image One"); ?>
+  <?php perch_content("Section Two"); ?>
+  <?php perch_content("Testimonials"); ?>
+  <?php perch_content("Contact Form"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('main-footer-end-js'); ?>
