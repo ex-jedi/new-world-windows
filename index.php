5cc7b4992f554f63e15016760810eead19d6f598
Mark <mark@pixelsmiths.co.uk>
2021-01-05 09:18:26 +0000

    Parallax image initial styles

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <?php perch_layout('main-header'); ?>
 <main class="main-content homepage-main-content" id="main-content">
   <?php perch_content("Section One"); ?>
-  <?php perch_content("Parallax Image One"); ?>
   <?php perch_content("Section Two"); ?>
+  <?php perch_content("Parallax Image"); ?>
   <?php perch_content("Testimonials"); ?>
   <?php perch_content("Contact Form"); ?>
 </main>
