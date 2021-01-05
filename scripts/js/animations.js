a37c9add177ac7a265a294fabfa736782968f806
Mark <mark@pixelsmiths.co.uk>
2021-01-05 18:40:48 +0000

    Registring GSAP plugins

--- scripts/js/animations.js
+++ scripts/js/animations.js
@@ -2,3 +2,5 @@
 import { gsap } from 'gsap';
 import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(CSSRulePlugin, ScrollTrigger);
