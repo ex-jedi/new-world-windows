bd7da1a5365fa180890b0dad2a903b768d9b5b35
Mark <mark@pixelsmiths.co.uk>
2021-01-10 17:23:42 +0000

    Services repeater

--- services.php
+++ services.php
@@ -1,8 +1,7 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content services-main-content" id="main-content">
-  <?php perch_content("Section One"); ?>
-  <?php perch_content("Section Two"); ?>
+  <?php perch_content("Services Content"); ?>
   <?php perch_content("Contact Form"); ?>
   <?php perch_content("Instagram Section"); ?>
 </main>
