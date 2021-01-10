90a506671a092044276c8a4497cd43438a57cc71
Mark <mark@pixelsmiths.co.uk>
2021-01-10 17:45:07 +0000

    About us setup

--- about-us.php
+++ about-us.php
@@ -1,10 +1,12 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
-<main class="main-content homepage-main-content" id="main-content">
-<h1>Ahoy</h1>
+<main class="main-content about-us-main-content" id="main-content">
+  <?php perch_content("About Us Content"); ?>
+  <?php perch_content("Contact Form"); ?>
+  <?php perch_content("Instagram Section"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
-<?php perch_layout('main-footer-end-js'); ?>
+<?php perch_layout('general-footer-end-js'); ?>
 <!-- TODO: Remove debug -->
 <!-- <?php PerchUtil::output_debug(); ?> -->
 
