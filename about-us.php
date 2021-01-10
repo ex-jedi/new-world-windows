63ae58e4c5cb599b99b6b6dd1c8197b673bd1a79
Mark <mark@pixelsmiths.co.uk>
2021-01-10 18:07:41 +0000

    Lazy loading images

--- about-us.php
+++ about-us.php
@@ -1,7 +1,8 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content about-us-main-content" id="main-content">
-  <?php perch_content("About Us Content"); ?>
+  <?php perch_content("About Us Intro"); ?>
+  <?php perch_content("Team Biographies"); ?>
   <?php perch_content("Contact Form"); ?>
   <?php perch_content("Instagram Section"); ?>
 </main>
