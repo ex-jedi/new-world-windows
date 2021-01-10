4267eb069da8a02c40a242ec0f30251cfa5f1f32
Mark <mark@pixelsmiths.co.uk>
2021-01-10 16:17:27 +0000

    Services pahe insta and form

--- services.php
+++ services.php
@@ -1,10 +1,13 @@
 <?php include('perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
-<main class="main-content homepage-main-content" id="main-content">
-<h1>Ahoy</h1>
+<main class="main-content services-main-content" id="main-content">
+  <?php perch_content("Section One"); ?>
+  <?php perch_content("Section Two"); ?>
+  <?php perch_content("Contact Form"); ?>
+  <?php perch_content("Instagram Section"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
-<?php perch_layout('main-footer-end-js'); ?>
+<?php perch_layout('general-footer-end-js'); ?>
 <!-- TODO: Remove debug -->
 <!-- <?php PerchUtil::output_debug(); ?> -->
 
