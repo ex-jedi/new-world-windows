468451c247438aee5ff5424f70a1cb589cd6ad84
Mark <mark@pixelsmiths.co.uk>
2021-01-11 08:25:07 +0000

    Blog templates

--- blog/index.php
+++ blog/index.php
@@ -1,6 +1,10 @@
 <?php include('../perch/runtime.php'); ?>
-<?php perch_layout('blog-header'); ?>
-  <main class="blog-main-content blog-index-main-content">
-    <?php perch_blog_recent_posts(5); ?>
-  </main>
-<?php perch_layout('blog-footer'); ?>
+<?php perch_layout('main-header'); ?>
+<main class="main-content services-main-content" id="main-content">
+     <?php perch_blog_recent_posts(5); ?>
+</main>
+<?php perch_layout('main-footer'); ?>
+<?php perch_layout('general-footer-end-js'); ?>
+<!-- TODO: Remove debug -->
+<!-- <?php PerchUtil::output_debug(); ?> -->
+
