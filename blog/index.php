d2e928c21297bdf2b1ab4f5c2689abd62912811b
Mark <mark@pixelsmiths.co.uk>
2021-01-11 08:35:14 +0000

    Contact from in blog index

--- blog/index.php
+++ blog/index.php
@@ -1,7 +1,8 @@
 <?php include('../perch/runtime.php'); ?>
 <?php perch_layout('main-header'); ?>
 <main class="main-content services-main-content" id="main-content">
-     <?php perch_blog_recent_posts(5); ?>
+  <?php perch_blog_recent_posts(5); ?>
+  <?php perch_content("Contact Form"); ?>
 </main>
 <?php perch_layout('main-footer'); ?>
 <?php perch_layout('general-footer-end-js'); ?>
