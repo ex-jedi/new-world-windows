e5a818d01cbf03cc1070d5fdf3b725f0915898a4
Mark <mark@pixelsmiths.co.uk>
2021-01-13 09:08:25 +0000

    Blog post header

--- perch/templates/layouts/blog-post-header.php
+++ perch/templates/layouts/blog-post-header.php
@@ -2,30 +2,27 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="/css/main.css">
-		<link rel="stylesheet" href="/css/blog.css">
-		<!-- Typekit  -->
-		<link rel="stylesheet" href="https://use.typekit.net/fmp5gzf.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="/css/main.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/css/main.css');?>">
+		<link rel="stylesheet" href="/css/blog.css?v=<?= filemtime($_SERVER['DOCUMENT_ROOT'].'/css/blog.css');?>">
+
+		<!-- Typekit -->
+		<link rel="stylesheet" href="https://use.typekit.net/ytg3fuz.css">
+
 		<!-- Perch Meta -->
-		<?php perch_blog_post_meta(perch_get('s'));
+	 	<?php perch_blog_post_meta(perch_get('s'));
 
 		perch_page_attributes(array(
 			'template' => 'favicons.html'
 		));
 
 		?>
-		<!-- Google Analytics -->
-		<?php perch_content('Analytics'); ?>
-
-<!-- Cookie Warning -->
-<!-- TODO: Move to footer -->
-<?php perch_content("Cookie Warning"); ?>
-
-	</head>
-	<body>
-	  <div class="site-wrapper blog-post-wrapper">
-		<header class="main-header blog-post-header">
+</head>
+	<body class="<?php perch_page_attribute('bodyClass', array('template' => 'bits.html' )); ?>" itemscope itemtype="http://schema.org/LocalBusiness">
+		<!-- Cookie Warning -->
+		<!-- TODO: Move to footer -->
+		<?php perch_content("Cookie Warning"); ?>
+	<header class="main-header blog-post-header">
 			<!-- Skip to main content link  -->
 			<a class="show-on-focus"  href="#main-content">Skip to main content</a>
 			<div class="header-nav-bar">
