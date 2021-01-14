d9e2770142bf4239c1d39adb17caac2a51f3bf11
Mark <mark@pixelsmiths.co.uk>
2021-01-14 16:31:43 +0000

    legals pages

--- privacy-and-cookie-policy.php
+++ privacy-and-cookie-policy.php
@@ -1,6 +1,5 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
-<?php perch_layout('main-header'); ?>
-<?php perch_layout('main-header-end'); ?>
+<?php perch_layout('legals-header'); ?>
 	<main class="legals privacy-policy">
 			<?php perch_content('Privacy And Cookie Policy'); ?>
 	</main>
--- terms.php
+++ terms.php
@@ -1,6 +1,5 @@
 <?php include('perch/runtime.php'); ?> <!-- Perch initialisation code -->
-<?php perch_layout('main-header'); ?>
-<?php perch_layout('main-header-end'); ?>
+<?php perch_layout('legals-header'); ?>
 <main class="legals terms-and conditions">
 		<?php perch_content('Terms And Conditions'); ?>
 </main>
