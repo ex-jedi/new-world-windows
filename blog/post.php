2ad6dafe4d312de5b210ffe3f870d8399ef4eacd
Mark <mark@pixelsmiths.co.uk>
2021-01-14 12:44:09 +0000

    Reframe working for blog posts

--- blog/post.php
+++ blog/post.php
@@ -1,10 +1,10 @@
 <?php include('../perch/runtime.php'); ?>
 <?php perch_layout('blog-post-header'); ?>
 	<main class="blog-main-content blog-post-main-content">
-		<article class="blog-post-article" itemscope itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
+		<article class="blog-post-article blog-section" itemscope itemtype="https://schema.org/BlogPosting" itemprop="blogPost">
 			<?php perch_blog_post(perch_get('s')); ?>
 
 		</article>
 	</main>
 <?php perch_layout('main-footer'); ?>
-<?php perch_layout('general-footer-end-js'); ?>
+<?php perch_layout('blogpost-footer-end-js'); ?>
---
+++ perch/templates/layouts/blogpost-footer-end-js.php
@@ -0,0 +1,4 @@
+<!-- JS file -->
+<script type="text/javascript" src="/scripts/minified/blog-post-min.js"></script>
+</body>
+</html>
